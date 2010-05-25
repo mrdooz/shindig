e6912af2622f3c8d3ecca64bd91ce2a205ecad55
Magnus Osterlind <dooz@spotify.com>
2010-05-25 17:57:04 +0200

    More on the obj loader

--- shindig/test_effect3.cpp
+++ shindig/test_effect3.cpp
@@ -5,6 +5,7 @@
 #include "scene.hpp"
 #include "mesh.hpp"
 #include "dynamic_vb.hpp"
+#include <celsus/text_scanner.hpp>
 #include <celsus/D3D11Descriptions.hpp>
 
 // wavefront obj loader
@@ -42,10 +43,66 @@ bool TestEffect3::render()
   return true;
 }
 
+struct Tri
+{
+	Tri(int a, int b, int c) : a(a), b(b), c(c) {}
+	int a, b, c;
+};
 
 bool TestEffect3::load_mesh(const std::string& filename)
 {
-  uint32_t len = 0;
-  char *buf = (char*)load_file(filename.c_str(), &len);
+	TextScanner scanner;
+	if (!scanner.load(filename.c_str()))
+		return false;
+
+
+	std::vector<D3DXVECTOR3> verts;
+	std::vector<Tri> tris;
+	std::set<int> invalid_verts;
+	std::set<int> invalid_tris;
+
+	verts.reserve(10000);
+	tris.reserve(10000);
+
+	int tri_idx = 0;
+	int vert_idx = 0;
+
+	while (true)
+	{
+		char ch;
+		if (!scanner.peek(&ch))
+			break;
+		switch(ch)
+		{
+		case 'v':
+			{
+				scanner.skip_chars("v \t");
+				std::vector<float> f;
+				scanner.read_floats(&f);
+				if (f.size() == 3)
+					verts.push_back(D3DXVECTOR3(f[0], f[1], f[2]));
+				else
+					invalid_verts.insert(vert_idx);
+				vert_idx++;
+			}
+			break;
+		case 'f':
+			{
+				scanner.skip_chars("f \t");
+				std::vector<int> i;
+				scanner.read_ints(&i);
+				if (i.size() == 3)
+					tris.push_back(Tri(i[0], i[1], i[2]));
+				else
+					invalid_tris.insert(tri_idx);
+				tri_idx++;
+			}
+			break;
+		default:
+			break;
+		}
+		scanner.skip_to_next_line();
+	}
+
   return true;
 }
